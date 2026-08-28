6eb56a5aa71533ebe8a3009656813497199093a4
Bahareh Gholami <bahareh.gh001@gmail.com>
2026-08-28 11:06:51 +0330

    install react-query devTools

--- app/providers/providers.tsx
+++ app/providers/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 
 
@@ -11,6 +12,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <QueryClientProvider client={queryClient}>
             {children}
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     )
 }
